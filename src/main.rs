a283a95dcd63ba98b291a9d439ef677538a0455d
Dylan Hicks <dman1366@gmail.com>
2017-12-04 13:43:21 -0700

    Seperate day outputs.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ mod day_2;
 use day_1::run_day_1;
 use day_2::run_day_2;
 
+fn print_seperator() {
+    println!("-------------------------------" );
+}
+
 fn main() {
     run_day_1();
+    print_seperator();
+
     run_day_2();
+    print_seperator();
+
+    // run_day_2();
+    // print_seperator();
 }
