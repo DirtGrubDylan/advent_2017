205d5a50c17caa59b7581f40375942817219a58d
Dylan Hicks <dman1366@gmail.com>
2017-12-04 10:39:21 -0700

    Get skeleton of day 2 up.

---
+++ src/day_2/mod.rs
@@ -0,0 +1,7 @@
+mod spreadsheet;
+
+use file_reader::to_string_vector;
+
+pub fn run_day_2() {
+    let day_2_input = to_string_vector("inputs/day_2.txt").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 pub mod file_reader;
 mod day_1;
+mod day_2;
 
 use day_1::run_day_1;
+use day_2::run_day_2;
 
 fn main() {
     run_day_1();
+    run_day_2();
 }
