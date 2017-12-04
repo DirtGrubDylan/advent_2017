a63e0a050728d479a05025aa332e63031860fd8d
Dylan Hicks <dman1366@gmail.com>
2017-12-04 10:24:11 -0700

    Rename part_1 to captcha, since part 1 and 2 are basically the same.

--- src/day_1/captcha.rs
+++ src/day_1/captcha.rs
@@ -10,11 +10,11 @@ impl Captcha {
         }
     }
 
-    pub fn sum(&self, steps_forward: usize) -> u32 {
+    pub fn sum(&self, step_size: usize) -> u32 {
         let chars = self.value.chars().collect::<Vec<char>>();
 
         chars.iter().enumerate().fold(0, |acc, (index, &c)| {
-            let next_index = (index + steps_forward) % self.value.len();
+            let next_index = (index + step_size) % self.value.len();
 
             let next_c = chars[next_index];
 
@@ -47,7 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn test_sum() {
+    fn test_sum_adjacent() {
         let test_captchas: Vec<Captcha> = to_string_vector("test_inputs/day_1_part_1.txt")
             .unwrap()
             .into_iter()
@@ -57,6 +57,22 @@ mod tests {
 
         for (captcha, expected_value) in test_captchas.iter().zip(expected_values.into_iter()) {
             assert_eq!(captcha.sum(1), expected_value);
+        }
+    }
+
+    #[test]
+    fn test_sum_half() {
+        let test_captchas: Vec<Captcha> = to_string_vector("test_inputs/day_1_part_2.txt")
+            .unwrap()
+            .into_iter()
+            .map(|value| Captcha::new(&value))
+            .collect();
+        let expected_values = vec![6, 0, 4, 12, 4];
+
+        for (captcha, expected_value) in test_captchas.iter().zip(expected_values.into_iter()) {
+            let step_size = captcha.value.len() / 2;
+
+            assert_eq!(captcha.sum(step_size), expected_value);
         }
     }
 }
