4faefd02c954a6446d9a789c55d9ae5f26fe7152
Dylan Hicks <dman1366@gmail.com>
2017-12-04 10:27:00 -0700

    Finish day 1.

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -4,11 +4,13 @@ use file_reader::to_string_vector;
 use self::captcha::Captcha;
 
 pub fn run_day_1() {
-    let day_1_part_1_input = to_string_vector("inputs/day_1.txt").unwrap();
+    let day_1_input = to_string_vector("inputs/day_1.txt").unwrap();
 
-    for input in day_1_part_1_input {
+    for input in day_1_input {
         let captcha = Captcha::new(&input);
+        let step_size = captcha.value.len() / 2;
 
         println!("Day 1, Part 1: {}", captcha.sum(1));
+        println!("Day 1, Part 2: {}", captcha.sum(step_size));
     }
 }
