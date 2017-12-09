71ad102796e8bd5b1b267eb58f07cd9ce8bdccc4
Dylan Hicks <dman1366@gmail.com>
2017-12-08 18:22:34 -0700

    Get day 5 skeleton finished.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_4;
+mod day_5;
 
 fn print_seperator() {
     println!("-------------------------------");
@@ -21,5 +22,8 @@ fn main() {
     print_seperator();
 
     day_4::run_day_4();
+    print_seperator();
+
+    day_5::run_day_5();
     print_seperator();
 }
