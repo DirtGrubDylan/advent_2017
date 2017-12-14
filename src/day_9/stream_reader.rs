ed9c4bed2788278be550a1265ac5318ef84624e2
Dylan Hicks <dman1366@gmail.com>
2017-12-14 16:43:18 -0700

    Continue.

--- src/day_9/stream_reader.rs
+++ src/day_9/stream_reader.rs
@@ -1,18 +1,41 @@
 use std::str::Chars;
 
-pub struct StreamReader<'a> {
-    inner: Chars<'a>,
+pub struct StreamReader {
+    inner: String,
 }
 
-impl<'a> StreamReader<'a> {
+impl StreamReader {
     pub fn new(stream: &str) -> StreamReader {
         StreamReader {
-            inner: stream.chars(),
+            inner: String::from(stream),
         }
     }
 
     pub fn total_group_score(&self) -> usize {
-        unimplemented!();
+        let mut depth = 1;
+        let mut score = 0;
+        let mut is_garbage = false;
+        let mut skip = false;
+
+        for c in self.inner.chars() {
+            match c {
+                '{' => {
+                    if !is_garbage {
+                        score += depth;
+                        depth += 1;
+                    }
+                }
+                '<' => {
+                    is_garbage = true;
+                }
+                '>' => {
+                    is_garbage = false;
+                }
+                _ => {}
+            }
+        }
+
+        score
     }
 }
 
@@ -25,9 +48,11 @@ mod tests {
     fn test_total_group_score() {
         let test_data = to_string_vector("test_inputs/day_9_part_1.txt").unwrap();
 
-        let test_scores: Vec<usize> =
-            test_data.iter().map(|s| StreamReader::new(s).total_group_score()).collect();
-        let expected = vec![1, 6, 5, 16, 1, 9, 9 ,3];
+        let test_scores: Vec<usize> = test_data
+            .iter()
+            .map(|s| StreamReader::new(s).total_group_score())
+            .collect();
+        let expected = vec![1, 6, 5, 16, 1, 9, 9, 3];
 
         assert_eq!(test_scores, expected);
     }
