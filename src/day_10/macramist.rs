8aaf02ac8d15c51a805f14b20680ea7c24d73d83
Dylan Hicks <dman1366@gmail.com>
2017-12-16 11:43:15 -0700

    Complete day 10!

--- src/day_10/macramist.rs
+++ src/day_10/macramist.rs
@@ -3,18 +3,102 @@ pub struct Macramist {
 }
 
 impl Macramist {
-    pub fn new() -> Macramist {
+    pub fn new(rope_length: usize) -> Macramist {
         Macramist {
-            inner: (0..256).collect(),
+            inner: (0..rope_length).collect(),
         }
     }
 
-    pub fn tie_with_lengths(&mut self, lengths: &[usize]) {
-        unimplemented!();
+    pub fn tie_with_lengths(
+        &mut self,
+        step: &mut usize,
+        current_position: &mut usize,
+        lengths: &[usize],
+    ) {
+        for &length in lengths {
+            let mut temp_vec = Vec::new();
+
+            for mut index in 0..length {
+                index = (*current_position + index) % self.inner.len();
+                temp_vec.push(self.inner[index]);
+            }
+
+            for mut index in 0..length {
+                index = (*current_position + index) % self.inner.len();
+                self.inner[index] = temp_vec.pop().unwrap();
+            }
+
+            *current_position += length + *step;
+            *step += 1;
+        }
+    }
+
+    pub fn hash_from_first_two(&self) -> usize {
+        self.inner[0] * self.inner[1]
+    }
+
+    pub fn xor_hash(&self) -> String {
+        let mut hash = String::new();
+
+        for chunk in self.inner.chunks(16) {
+            let mut temp_hash = format!("0{:x}", chunk.iter().fold(0, |acc, x| acc ^ x));
+
+            if temp_hash.len() == 3 {
+                hash += &temp_hash[1..3];
+            } else {
+                hash += temp_hash.as_str();
+            }
+        }
+
+        hash
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_tie_with_lengths() {
+        let mut step = 0;
+        let mut current_position = 0;
+        let mut test_macramist = Macramist::new(5);
+
+        test_macramist.tie_with_lengths(&mut step, &mut current_position, &[3, 4, 1, 5]);
+
+        assert_eq!(test_macramist.inner, vec![3, 4, 2, 1, 0]);
+    }
+
+    #[test]
+    fn test_hash_from_first_two() {
+        let mut step = 0;
+        let mut current_position = 0;
+        let mut test_macramist = Macramist::new(5);
+
+        assert_eq!(test_macramist.hash_from_first_two(), 0);
+
+        test_macramist.tie_with_lengths(&mut step, &mut current_position, &[3, 4, 1, 5]);
+
+        assert_eq!(test_macramist.hash_from_first_two(), 12);
+    }
+
+    #[test]
+    fn test_xor_hash() {
+        let mut step = 0;
+        let mut current_position = 0;
+        let mut test_macramist = Macramist::new(256);
+
+        for _ in 0..64 {
+            test_macramist.tie_with_lengths(
+                &mut step,
+                &mut current_position,
+                &[49, 44, 50, 44, 51, 17, 31, 73, 47, 23],
+            );
+        }
+
+        assert_eq!(
+            test_macramist.xor_hash(),
+            String::from("3efbe78a8d82f29979031a4aa0b16a9d")
+        );
+    }
 }
--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,3 +1,31 @@
 mod macramist;
 
-pub fn run_day_10() {}
+use file_reader::to_string_vector;
+use self::macramist::Macramist;
+
+pub fn run_day_10() {
+    let input = to_string_vector("inputs/day_10.txt").unwrap();
+    let lengths: Vec<usize> = input[0].split(',').map(|s| s.parse().unwrap()).collect();
+
+    let mut step = 0;
+    let mut current_position = 0;
+    let mut macramist = Macramist::new(256);
+
+    macramist.tie_with_lengths(&mut step, &mut current_position, &lengths);
+
+    println!("Day 10, Part 1: {}", macramist.hash_from_first_two());
+
+    let mut ascii_lengths: Vec<usize> = input[0].chars().map(|c| c as usize).collect();
+
+    ascii_lengths.extend_from_slice(&[17, 31, 73, 47, 23]);
+
+    step = 0;
+    current_position = 0;
+    macramist = Macramist::new(256);
+
+    for _ in 0..64 {
+        macramist.tie_with_lengths(&mut step, &mut current_position, &ascii_lengths);
+    }
+
+    println!("Day 10, Part 2: {}", macramist.xor_hash());
+}
